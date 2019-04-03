ef06e9365447c742bb99bb94e7bf1dac28e7fe53
Jean Perrault <perrault.jean66gmail.com>
2019-04-03 11:36:37 +0200

    -

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -9,20 +9,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class WelcomeController extends AbstractController
 {
     /**
-     * @Route("/hello", name="hello")
+     * @Route("/", name="homepage")
      */
-    public function hello()
+    public function home()
     {
-        $name = 'Jean';
+        return $this->render('welcome/index.html.twig');
+    }
+    /**
+     * @Route(
+     *      "/hello/{name}",
+     *      name="hello",
+     *      requirements={"name":"^[a-z]{3,8}$"}
+     * )
+     */
+    public function hello($name = 'jean')
+    {
+        
 
         // On peut dumper une variable comme var_dump
         dump($name);
 
-        return $this->render('Welcome/hello.html.twig',[
-            'name' => $name
+        return $this-> render('Welcome/hello.html.twig',[
+            'name' => ucfirst($name)
         ]);
 
-        return new Response('<html><body>Hello '.$name.'</body></html>');
+        
       
-;    }
+    }
 }
